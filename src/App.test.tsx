23334836269edab4e826bdc9854d543d87c567cf
Todor Grigorov <todor.grigorov@abraxa.com>
2024-11-05 14:18:39 +0200

    initial

---
+++ src/App.test.tsx
@@ -0,0 +1,7 @@
+import {test} from 'vitest'
+import { render } from 'vitest-browser-react';
+import App from './App';
+
+test('App', () => {
+	render(<App />)
+})
---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    browser: {
+      enabled: true,
+      name: "chromium",
+      headless: true,
+      provider: "playwright",
+      screenshotFailures: false,
+    },
+  },
+});
